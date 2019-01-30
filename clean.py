12add35464f2541e0862dffd0df4ca8482cf4582
Robert Lee <rlee32@gatech.edu>
2019-01-30 04:39:00 -0500

    improved clean script, removed accidentally added generated files

--- clean.py
+++ clean.py
@@ -6,11 +6,10 @@ folders = [x for x in os.listdir("case/") if os.path.isdir("case/" + x)]
 for f in folders:
     dotsep = f.split(".")
     isfloat = len(dotsep) > 1 and all([x.isdigit() for x in dotsep])
-    esep = f.split("e-")
-    issci = len(esep) > 1 and all([x.isdigit() for x in esep])
+    issci = "e-" in f
     isnum = f.isdigit() or isfloat or issci
     if isnum and float(f) != 0:
         os.system("rm -r case/" + f)
-os.system("rm main.msh")
-os.system("rm -r case/constant/polyMesh")
+os.system("rm -f main.msh")
+os.system("rm -rf case/constant/polyMesh")
 
